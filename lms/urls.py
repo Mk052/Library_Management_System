64c8b32dd1b30f75250aaed6d4e839fde0145611
madhu <madhukush006@gmail.com>
2025-03-13 13:29:46 +0530

    Implemented the Login functionality using Simple JWT authentication

--- lms/urls.py
+++ lms/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from lms.views import (UserRegistrationAPIView)
+from lms.views import (UserRegistrationAPIView, UserLoginAPIView)
 
 urlpatterns = [
     path("signup/", UserRegistrationAPIView.as_view()),
+    path("login/", UserLoginAPIView.as_view()),
 ]
--- lms/views.py
+++ lms/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 
 from lms.models import (User, Student)
 from lms.serializers import (UserSerializer)
+from lms.utils import get_tokens_for_user
 
 
 class UserRegistrationAPIView(generics.GenericAPIView):
@@ -21,3 +22,16 @@ class UserRegistrationAPIView(generics.GenericAPIView):
     def perform_create(self, serializer):
         user = serializer.save()
         Student.objects.create(user=user)
+
+
+class UserLoginAPIView(generics.GenericAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def post(self, request, *args, **kwargs):
+        email = self.request.data.get("email")
+        user = User.objects.filter(email=email).first()
+        if not user:
+            return Response({"msg": "User not found"})
+        response = get_tokens_for_user(user)
+        return Response(response)
