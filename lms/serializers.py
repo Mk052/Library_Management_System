aac51b0e33df6d42321270e9fab670cce49e199e
madhu <madhukush006@gmail.com>
2025-03-13 14:03:12 +0530

    Added BookSerializer class in serializers.py

--- lms/serializers.py
+++ lms/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from lms.models import (User, Author, Category)
+from lms.models import (User, Author, Category, Book)
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -49,3 +49,29 @@ class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ["id", "name"]
+
+
+class BookSerializer(serializers.ModelSerializer):
+    id = serializers.ReadOnlyField()
+    author_id = serializers.PrimaryKeyRelatedField(
+        queryset=Author.objects.all(), write_only=True, source="author"
+    )
+    author = serializers.StringRelatedField(read_only=True)
+    category_id = serializers.PrimaryKeyRelatedField(
+        queryset=Category.objects.all(), write_only=True, source="category"
+    )
+    category = serializers.StringRelatedField(read_only=True)
+
+    class Meta:
+        model = Book
+        fields = [
+            "id",
+            "title",
+            "description",
+            "author",
+            "category",
+            "total_book",
+            "book_copies",
+            "author_id",
+            "category_id",
+        ]
