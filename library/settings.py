8acb857d0ff9c1dcb9e5649fc3e640c785fab6ba
MK052 <madhukush006@gmail.com>
2025-05-08 15:24:51 +0530

    update the requirements.txt file

--- library/settings.py
+++ library/settings.py
@@ -103,16 +103,16 @@ WSGI_APPLICATION = "library.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": os.getenv("DATABASE_NAME"),
-#         "USER": os.getenv("DATABASE_USER"),
-#         "PASSWORD": os.getenv("DATABASE_PASSWORD"),
-#         "HOST": os.getenv("DATABASE_HOST", "127.0.0.1"),
-#         "PORT": os.getenv("DATABASE_PORT", "5432"),
-#     }
-# }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DATABASE_NAME"),
+        "USER": os.getenv("DATABASE_USER"),
+        "PASSWORD": os.getenv("DATABASE_PASSWORD"),
+        "HOST": os.getenv("DATABASE_HOST", "127.0.0.1"),
+        "PORT": os.getenv("DATABASE_PORT", "5432"),
+    }
+}
 # DATABASES = {
 #     "default": dj_database_url.config(default="postgres://localhost")
 # }
